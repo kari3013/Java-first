4c6f3875d9bc94b58a1994c62d591f857e79d11a
karishma patel <krpatel2kr6@yahoo.com>
2023-02-17 13:08:21 +0000

    my java first

---
+++ Firstpacticeclass.java
@@ -0,0 +1,18 @@
+package manualtopic;
+
+public class Firstpacticeclass {
+    public static void main(String[] args)
+    {
+        System.out.println("First Java programe");
+        System.out.println("Become Software Tester");
+        System.out.println("Software testing");
+        System.out.println("Manual Testing");
+        System.out.println("Application Software");
+        System.out.println("System software");
+        System.out.println("Product based company");
+        System.out.println("Service Based Company");
+        System.out.println("Risks of testing");
+        System.out.println("Advantages of Testing");
+
+    }
+}
